fc780e3a3a8d5e8789aa8bb1449a1dc94e528bf6
Son <j.son5457@gmail.com>
2023-07-14 14:57:20 -0500

    Updates for manuscript

--- behavioral_analyses.py
+++ behavioral_analyses.py
@@ -11,6 +11,30 @@ df = pd.read_csv('DevMIND_Flanker_Hormone_Preprocessing_v3.csv')
 df = df[df['Excluded'] == 'N']
 df = df[df['T_mean'] > 0]
 
+########################################################################################################################
+# Behavioral Results - Summary Stats
+
+np.mean(df['Accuracy_Control'])
+np.std(df['Accuracy_Control'])
+
+np.mean(df['Total_Flanker'])
+np.std(df['Total_Flanker'])
+
+np.mean(df['RT_Control'])
+np.std(df['RT_Control'])
+
+np.mean(df['RT_Flanker'])
+np.std(df['RT_Flanker'])
+
+########################################################################################################################
+# Behavioral Results - Condition-wise Testing
+
+scipy.stats.ttest_rel(df['Accuracy_Control'], df['Total_Flanker'])
+scipy.stats.ttest_rel(df['RT_Flanker'], df['RT_Control'])
+
+########################################################################################################################
+# Behavioral Results - Correlations with Testosterone
+
 age_testosterone = scipy.stats.pearsonr(df['T_mean'], df['age'])
 
 m, b = np.polyfit(df['T_mean'], df['age'], 1)
@@ -20,9 +44,11 @@ plt.scatter(df['T_mean'], df['age'])
 plt.plot(df['T_mean'], m*df['T_mean']+b)
 plt.show()
 
-df['RT_avg'] = (df['Total_Control']*df['RT_Control'] + df['Total_Flanker']*df['RT_Flanker'])/(df['Total_Control'] + df['Total_Flanker'])
+df['RT_avg'] = (df['Accuracy_Control']*100*df['RT_Control'] + df['Total_Flanker']*df['RT_Flanker'])/(df['Accuracy_Control']*100 + df['Total_Flanker'])
 
-testosterone_behavior = scipy.stats.pearsonr(df['RT_avg'], df['T_mean'])
+testosterone_RT = scipy.stats.pearsonr(df['RT_avg'], df['T_mean'])
+testosterone_RT = scipy.stats.pearsonr(df['RT_Control'], df['T_mean'])
+testosterone_RT = scipy.stats.pearsonr(df['RT_Flanker'], df['T_mean'])
 
 m, b = np.polyfit(df['T_mean'], df['RT_avg'], 1)
 
@@ -31,3 +57,37 @@ plt.scatter(df['T_mean'], df['RT_avg'])
 plt.plot(df['T_mean'], m*df['T_mean']+b)
 plt.show()
 
+df['Acc_avg'] = (df['Accuracy_Control']+df['Total_Flanker'])/200
+
+testosterone_acc = scipy.stats.pearsonr(df['Acc_avg'], df['T_mean'])
+
+m, b = np.polyfit(df['T_mean'], df['Acc_avg'], 1)
+
+plt.figure()
+plt.scatter(df['T_mean'], df['Acc_avg'])
+plt.plot(df['T_mean'], m*df['T_mean']+b)
+plt.show()
+
+testosterone_interference = scipy.stats.pearsonr(df['RT_int'], df['T_mean'])
+
+m, b = np.polyfit(df['T_mean'], df['RT_int'], 1)
+
+plt.figure()
+plt.scatter(df['T_mean'], df['RT_int'])
+plt.plot(df['T_mean'], m*df['T_mean']+b)
+plt.show()
+
+########################################################################################################################
+# MEG Sensor-Level Results
+
+scipy.stats.ttest_rel(df['Accepted_Control'], df['Accepted_Flanker_Adjusted'])
+scipy.stats.pearsonr(df['Accepted_Total_Trials'], df['T_mean'])
+
+np.mean(df['Accepted_Control'])
+np.std(df['Accepted_Control'])
+
+np.mean(df['Accepted_Flanker_Adjusted'])
+np.std(df['Accepted_Flanker_Adjusted'])
+
+np.mean(df['Accepted_Total_Trials'])
+np.std(df['Accepted_Total_Trials'])
